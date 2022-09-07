5d2aaa75dd47b4811e230de4a9bad8b81785678b
Ubuntu <ubuntu@ip-172-31-29-210.ec2.internal>
2022-09-07 14:54:18 +0000

    adding shell scripts

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mmaxwellc/maxwell
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run v1\
+    --image=$dockerpath\
+    --port=80 --labels app=mldemo
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward v1 8000:80
